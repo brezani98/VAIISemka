adb359e874dc71656e37ba7b5156fc2acc606ff4
Anton Brezáni <antonbrezani@gmail.com>
2021-01-15 11:22:25 +0100

    Prazdny projekt

---
+++ VAIISemka/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace VAIISemka.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
